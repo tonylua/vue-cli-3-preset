59ebf93d621839dfdada0434f1b9232866328428
tonylua <tonylua@sina.com>
2019-06-26 11:09:09 +0800

    optimize bundle

--- template/config/plugins.chain.js
+++ template/config/plugins.chain.js
@@ -6,5 +6,18 @@ module.exports = (config) => {
     .use(webpack.ProvidePlugin, [{
       //'$': 'jquery',
     }]);
+    
+  if (process.env.NODE_ENV === 'production') {
+    // config.externals({
+    // });
+  
+    // https://webpack.js.org/plugins/split-chunks-plugin/
+    config.optimization.splitChunks({
+      chunks: 'async',
+      maxAsyncRequests: 5,
+      maxSize: 1500000,
+      minSize: 1000000,
+    });
+  }
   return config; 
 };
