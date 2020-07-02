4c76aa7c06cc6f60ada5e394277bafbfc96b4bf1
tonylua <tonylua@sina.com>
2020-07-02 22:27:00 +0800

    update dep

--- generator/helpers/composition.helper.js
+++ generator/helpers/composition.helper.js
@@ -1,10 +1,12 @@
 module.exports = (api) => {
-	api.extendPackage({
-	  devDependencies: {
-      "@vue/composition-api": "^0.3.4"
-	  }
-	});
-  api.render('../../custom-deps/composition-api');
-	api.injectImports(api.entryFile,
-	  `import VueCompositionApi from '@vue/composition-api'`);
+  api.extendPackage({
+    devDependencies: {
+      "@vue/composition-api": "^1.0.0-beta.1",
+    },
+  });
+  api.render("../../custom-deps/composition-api");
+  api.injectImports(
+    api.entryFile,
+    `import VueCompositionApi from '@vue/composition-api'`
+  );
 };
