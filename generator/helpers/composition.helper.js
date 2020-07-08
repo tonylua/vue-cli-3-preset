cc8a638e65ba37784c45fe2c30ff222f3eea0061
tonylua <tonylua@sina.com>
2020-07-08 21:51:35 +0800

    fix

--- generator/helpers/composition.helper.js
+++ generator/helpers/composition.helper.js
@@ -4,7 +4,7 @@ module.exports = (api) => {
       "@vue/composition-api": "^1.0.0-beta.1",
     },
   });
-  api.render("../../custom-deps/composition-api");
+  // api.render("../../custom-deps/composition-api");
   api.injectImports(
     api.entryFile,
     `import VueCompositionApi from '@vue/composition-api'`
