79ba6472476567b0e1f87eaf102dd70ae152dc73
tonylua <tonylua@sina.com>
2020-07-11 11:35:34 +0800

    add alias

--- template/config/resolve.chain.js
+++ template/config/resolve.chain.js
@@ -1,12 +1,12 @@
-const path = require('path');
+const path = require("path");
 
-const resolve = (dir) => path.join(__dirname, '..', dir);
+const resolve = (dir) => path.join(__dirname, "..", dir);
 
 module.exports = (config) => {
-  config.resolve
-    .set('extensions', ['.js', '.vue', '.json', '.css', '.scss']);
+  config.resolve.set("extensions", [".js", ".vue", ".json", ".css", ".scss"]);
   config.resolve.alias
-    .set('~', resolve('node_modules'))
-    .set('@', resolve('src'));
+    .set("~", resolve("node_modules"))
+    .set("assets", resolve("src/assets")) // use for css: ... url('~assets/xxx')
+    .set("@", resolve("src"));
   return config;
 };
