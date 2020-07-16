41a87e66a073a672875a67802d0878840ef27f71
tonylua <tonylua@sina.com>
2020-07-17 00:13:21 +0800

    fix storybook + ts

--- custom-deps/storybook/_storybook/webpack.config.js
+++ custom-deps/storybook/_storybook/webpack.config.js
@@ -1,31 +1,54 @@
-const path = require('path');
-const pathResolve = p => path.resolve(__dirname, '../', p);
+const path = require("path");
+const pathResolve = (p) => path.resolve(__dirname, "../", p);
 module.exports = ({ config, mode }) => {
+  config.resolve.extensions.push(
+    ".ts",
+    ".tsx",
+    ".vue",
+    ".css",
+    ".less",
+    ".scss",
+    ".sass",
+    ".html"
+  );
   config.resolve.alias = {
     ...config.resolve.alias,
-    '@': pathResolve('src'),
-    '~': pathResolve('node_modules')
+    "@": pathResolve("src"),
+    assets: pathResolve("src/assets"),
+    "~": pathResolve("node_modules"),
   };
   config.module.rules.push({
     test: /\.scss$/,
-    include: pathResolve('src'),
-    use: ['style-loader', 'css-loader', 'sass-loader'],
+    include: pathResolve("src"),
+    use: ["style-loader", "css-loader", "sass-loader"],
   });
   config.module.rules.push({
     test: /\.vue$/,
-    loader: 'storybook-addon-vue-info/loader',
-    enforce: 'post'
+    loader: "storybook-addon-vue-info/loader",
+    enforce: "post",
   });
   config.module.rules.push({
     test: /\.stories\.js$/,
-    loaders: [require.resolve('@storybook/addon-storysource/loader')],
-    enforce: 'pre'
+    loaders: [require.resolve("@storybook/addon-storysource/loader")],
+    enforce: "pre",
   });
-  if (process.env.NODE_ENV === 'production') {
-    config.output.filename = 'bundle.[name].js';
+  config.module.rules.push({
+    test: /\.ts$/,
+    exclude: /node_modules/,
+    use: [
+      {
+        loader: "ts-loader",
+        options: {
+          appendTsSuffixTo: [/\.vue$/],
+        },
+      },
+    ],
+  });
+  if (process.env.NODE_ENV === "production") {
+    config.output.filename = "bundle.[name].js";
     config.optimization.splitChunks.automaticNameDelimiter = ".";
     config.optimization.runtimeChunk = {
-      name: entrypoint => `runtime.${entrypoint.name}`
+      name: (entrypoint) => `runtime.${entrypoint.name}`,
     };
   }
   // console.log(config);
