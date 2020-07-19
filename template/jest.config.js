f2202beb930d6907207714823b47df89b5b40bd5
tonylua <tonylua@sina.com>
2020-07-19 18:26:07 +0800

    optimize jest

--- template/jest.config.js
+++ template/jest.config.js
@@ -1,32 +1,34 @@
 module.exports = {
-  modulePaths: ['<rootDir>/src/components', '<rootDir>/src/utils'],
+  etupFiles: ["<rootDir>/__mocks__/localStorageMock.js"],
+  modulePaths: ["<rootDir>/src/components", "<rootDir>/src/utils"],
 
-  moduleFileExtensions: ['js', 'json', 'jsx', 'vue', 'ts', 'tsx'],
+  moduleFileExtensions: ["js", "json", "jsx", "vue", "ts", "tsx"],
 
   transform: {
-    '^.+\\.vue$': 'vue-jest',
-    '^.+\\.jsx?$': 'babel-jest',
-    '^.+\\.tsx?$': 'ts-jest'
+    "^.+\\.vue$": "vue-jest",
+    "^.+\\.jsx?$": "babel-jest",
+    "^.+\\.tsx?$": "ts-jest",
   },
 
-  transformIgnorePatterns: ['/node_modules/'],
+  transformIgnorePatterns: ["/node_modules/"],
 
   moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1',
-    '\\.(css|less|scss)$': '<rootDir>/__mocks__/emptyMock.js'
+    "^@/(.*)$": "<rootDir>/src/$1",
+    "\\.(css|less|scss)$": "<rootDir>/__mocks__/emptyMock.js",
   },
 
-  snapshotSerializers: ['jest-serializer-vue'],
+  snapshotSerializers: ["jest-serializer-vue"],
 
   collectCoverage: true,
 
   collectCoverageFrom: [
-    '<rootDir>/src/components/**/*.{ts,js,vue}',
-    '<rootDir>/src/utils/**/*.{ts,js,vue}',
-    '!**/node_modules/**'
+    "<rootDir>/src/components/**/*.{ts,js,vue}",
+    "<rootDir>/src/utils/**/*.{ts,js,vue}",
+    "!**/*.stories.js",
+    "!**/node_modules/**",
   ],
 
-  coveragePathIgnorePatterns: ['<rootDir>/__tests__/'],
+  coveragePathIgnorePatterns: ["<rootDir>/__tests__/"],
 
-  coverageReporters: ['text']
+  coverageReporters: ["text"],
 };
