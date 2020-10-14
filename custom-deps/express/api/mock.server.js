6e9eb5787ba20dc55322f55ef2e721255065ec66
tonylua <tonylua@sina.com>
2020-10-14 23:19:12 +0800

    optimize

--- custom-deps/express/api/mock.server.js
+++ custom-deps/express/api/mock.server.js
@@ -1,81 +1,84 @@
-const express = require('express');
-const qs = require('qs');
-const bodyParser = require('body-parser');
-const request = require('request');
-const path = require('path');
-const walk = require('klaw-sync');
-const _ = require('lodash');
-const config = require('../config/config');
+const express = require("express");
+const qs = require("qs");
+const bodyParser = require("body-parser");
+const request = require("request");
+const path = require("path");
+const walk = require("klaw-sync");
+const _ = require("lodash");
+const config = require("../config/config");
 
 const argRe = /^\-{1,2}([a-z]+?)=(.*)$/;
-const argsMap = process.argv.splice(2).reduce(
-  (map, argStr) => {
-    if (argRe.test(argStr)) {
-      const [_, key, value] = argStr.match(argRe);
-      map[key] = value;
-    }
-    return map;
-  },
-  Object.create(null)
-);
+const argsMap = process.argv.splice(2).reduce((map, argStr) => {
+  if (argRe.test(argStr)) {
+    const [_, key, value] = argStr.match(argRe);
+    map[key] = value;
+  }
+  return map;
+}, Object.create(null));
 
-const {
-  mock,
-  proxy
-} = config;
+const { mock, proxy } = config;
 
 // console.log(proxy);
 
-process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0; // eslint-disable-line
+process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0; // eslint-disable-line
 
 const app = express();
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
 app.use(bodyParser.json());
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', argsMap.mockorigin || '*');
-  res.header('Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, hci-secret-key, x-api-key');
-  res.header('Access-Control-Allow-Methods', 'POST, OPTIONS, GET');
-  res.header('Access-Control-Allow-Credentials', true);
+  res.header("Access-Control-Allow-Origin", argsMap.mockorigin || "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, hci-secret-key, x-api-key"
+  );
+  res.header("Access-Control-Allow-Methods", "POST, OPTIONS, GET");
+  res.header("Access-Control-Allow-Credentials", true);
   next();
 });
 
 let _existRoutes = [];
-const isSavedURLMatch = (p) => {
-  if (~_existRoutes.indexOf(p)) return true;
-  return _existRoutes.some(r => { // look up for route like "/foo/:id"
-    if (!~r.indexOf(':')) return false;
-    const str = r.replace(/:.+?($|\/)/g, '.*?$1')
-      .replace(/(\/{1})/g, '\\$1');
+const isSavedURLMatch = (m, p) => {
+  const tmpStr = `${m.toUpperCase()} ${p}`;
+  if (~_existRoutes.indexOf(tmpStr)) return true;
+  return _existRoutes.some((r) => {
+    // look up for route like "/foo/:id"
+    if (!~r.indexOf(":")) return false;
+    const str = r
+      .replace(/^[A-Z]+\s/, "")
+      .replace(/:.+?($|\/)/g, ".*?$1")
+      .replace(/(\/{1})/g, "\\$1");
     const re = new RegExp(`^${str}$`);
     return re.test(p);
   });
 };
 app.use((req, res, next) => {
-  if (!isSavedURLMatch(req.path)) {
+  if (!isSavedURLMatch(req.method, req.path)) {
     let url = req.originalUrl;
-    let target = '';
+    let target = "";
     for (let i = 0; i < proxy.length; i++) {
       const p = proxy[i];
-      if (url.substr(0, p.prefix.length) === p.prefix
-        && p.target.length > target.length) { // '/api/v1' rather than '/api'
-          target = p.target;
-        }
+      if (
+        url.substr(0, p.prefix.length) === p.prefix &&
+        p.target.length > target.length
+      ) {
+        // '/api/v1' rather than '/api'
+        target = p.target;
+      }
     }
-    target = target.replace(/\/+$/, '');
-    url = url.replace(/^\/+/, '');
+    target = target.replace(/\/+$/, "");
+    url = url.replace(/^\/+/, "");
     const rUrl = `${target}/${url}`;
     const rOptions = {
       url: rUrl,
       method: req.method,
-			headers: _.omit(req.headers, [
-				'host'
-			]),
-			jar: true
+      headers: _.omit(req.headers, ["host"]),
+      jar: true,
     };
-    console.log('[redirect]', req.method, target, url, req.body);
+    console.log("[redirect]", req.method, target, url, req.body);
 
     if (/GET|HEAD|DELETE/i.test(req.method)) {
       req.pipe(request(rOptions)).pipe(res);
@@ -83,15 +86,15 @@ app.use((req, res, next) => {
       return;
     }
 
-    const bodyKey = ~req.headers['content-type'].indexOf('application/json')
-      ? 'json'
-      : 'form';
-    if (req.body && req.headers['content-length'] > 0) {
+    const bodyKey = ~req.headers["content-type"].indexOf("application/json")
+      ? "json"
+      : "form";
+    if (req.body && req.headers["content-length"] > 0) {
       rOptions[bodyKey] = req.body;
     } else {
-      const fakeBody = { '__timestamp1' : Date.now() };
+      const fakeBody = { __timestamp1: Date.now() };
       rOptions[bodyKey] = fakeBody;
-      rOptions.headers['content-length'] = qs.stringify(fakeBody).length;
+      rOptions.headers["content-length"] = qs.stringify(fakeBody).length;
     }
     req.pipe(request(rOptions), { end: false }).pipe(res);
   }
@@ -99,11 +102,22 @@ app.use((req, res, next) => {
 });
 
 walk(__dirname)
-  .filter(p => /\.api\.js$/.test(p.path))
-  .map(p => p.path)
-  .forEach(part => require(part)(app));
+  .filter((p) => /\.api\.js$/.test(p.path))
+  .map((p) => p.path)
+  .forEach((part) => require(part)(app, config.prefix));
 
-_existRoutes = app._router.stack.filter(s => s.route).map(s => s.route.path);
+_existRoutes = app._router.stack
+  .filter((s) => s.route)
+  .map((s) => {
+    const { path, stack } = s.route;
+    let m = "";
+    try {
+      m = `${stack[0].method.toUpperCase()} `;
+    } catch (ex) {
+      console.log(ex);
+    }
+    return `${m}${path}`;
+  });
 console.log(_existRoutes.sort());
 
 const { mockport, mockhost } = argsMap;
--- generator/helpers/storybook.helper.js
+++ generator/helpers/storybook.helper.js
@@ -1,27 +1,27 @@
 module.exports = (api, ANSWERS) => {
-	api.extendPackage({
+  api.extendPackage({
     scripts: {
-      "storybook": "start-storybook -p 6006",
-      "build-storybook": "build-storybook"
+      storybook: "start-storybook -p 6006",
+      "build-storybook": "build-storybook",
     },
-	  devDependencies: {
+    devDependencies: {
       "babel-core": "7.0.0-bridge.0",
       "babel-preset-vue": "^2.0.2",
-      "node-sass": "^4.9.0",
+      "node-sass": "^4.14.1",
       "sass-loader": "^8.0.0",
       "vue-loader": "^15.7.0",
       "vue-style-loader": "^4.1.2",
       "vue-template-compiler": "^2.6.10",
-	    "@storybook/addon-backgrounds": "^5.3.14",
-	    "@storybook/addon-storysource": "^5.3.14",
-	    "@storybook/addon-viewport": "^5.3.14",
-	    "@storybook/addons": "^5.3.14",
-	    "@storybook/vue": "^5.3.14",
-      "storybook-addon-vue-info": "^1.4.1"
-	  }
-	});
-	api.render('../../custom-deps/storybook', {
-	  elementUI: ANSWERS['element-ui'],
-    compositionAPI: ANSWERS['composition-api']
-	});
+      "@storybook/addon-backgrounds": "^5.3.14",
+      "@storybook/addon-storysource": "^5.3.14",
+      "@storybook/addon-viewport": "^5.3.14",
+      "@storybook/addons": "^5.3.14",
+      "@storybook/vue": "^5.3.14",
+      "storybook-addon-vue-info": "^1.4.1",
+    },
+  });
+  api.render("../../custom-deps/storybook", {
+    elementUI: ANSWERS["element-ui"],
+    compositionAPI: ANSWERS["composition-api"],
+  });
 };
